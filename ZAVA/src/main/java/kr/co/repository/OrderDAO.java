3528b853595405379e557421e811b98c876b9842
musmdaekiri <ghkdldj13@gmail.com>
2022-01-05 18:04:08 +0900

    주문수정끝0107

---
+++ ZAVA/src/main/java/kr/co/repository/OrderDAO.java
@@ -0,0 +1,22 @@
+package kr.co.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import kr.co.domain.OrderDetailVO;
+import kr.co.domain.OrderVO;
+
+@Repository
+public interface OrderDAO {
+
+
+	void order(OrderVO vo);
+
+
+	void orderDetail(OrderDetailVO odVo);
+
+
+	List<OrderVO> orderList(OrderVO oVo);
+	
+}
